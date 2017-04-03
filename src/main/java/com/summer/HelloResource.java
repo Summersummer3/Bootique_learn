effc134109cee663c7b038dc0193784ac41a836d
summer <techsum8@live.com>
2017-04-03 22:03:19 +0900

    test

--- src/main/java/com/summer/HelloResource.java
+++ src/main/java/com/summer/HelloResource.java
@@ -1,14 +1,26 @@
 package com.summer;
+import com.google.inject.Inject;
+import io.bootique.annotation.Args;
+
+import static java.util.stream.Collectors.joining;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.util.Arrays;
+
 /**
  * Created by summer on 2017/3/27.
  */
 
 @Path("/")
 public class HelloResource {
+    @Inject
+    @Args
+    private String[] args;
+
     @GET
     public String hello(){
-        return "hello, world!";
+        String allArgs = Arrays.asList(args).stream().collect(joining(" "));
+        return "hello, world! The app was started with the following arguments: " + allArgs;
     }
 }
