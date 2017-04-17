929826bd4137bfd81d0fc5a74561eddd9fb4a9c4
summer <techsum8@live.com>
2017-04-17 22:36:29 +0900

    add json

--- src/main/java/com/summer/HelloResource.java
+++ src/main/java/com/summer/HelloResource.java
@@ -2,10 +2,11 @@ package com.summer;
 import com.google.inject.Inject;
 import io.bootique.annotation.Args;
 
-import static java.util.stream.Collectors.joining;
-
 import javax.ws.rs.*;
-import java.util.Arrays;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by summer on 2017/3/27.
@@ -17,10 +18,14 @@ public class HelloResource {
     @Args
     private String[] args;
 
+//    @GET
+//    public String hello(){
+//        String allArgs = Arrays.asList(args).stream().collect(joining(" "));
+//        return "hello, world! The app was started with the following arguments: " + allArgs;
+//    }
     @GET
-    public String hello(){
-        String allArgs = Arrays.asList(args).stream().collect(joining(" "));
-        return "hello, world! The app was started with the following arguments: " + allArgs;
+    public Response sayHelloWorld(){
+        return Response.ok("hello!!").type(MediaType.TEXT_PLAIN_TYPE).build();
     }
 
     @GET
@@ -34,4 +39,70 @@ public class HelloResource {
         return sb.toString();
     }
 
+    @POST
+    @Consumes("text/plain")
+    @Path("/echo")
+    public String sayHelloEcho(String message){
+        return "Hello! "+message;
+    }
+
+    private static Map<Integer, JsonTest> tests = new HashMap<>();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/save")
+    public MyResponse saveName(JsonTest t){
+        tests.put(t.getId(), t);
+        MyResponse res = new MyResponse();
+        res.setMessage("OK");
+        return res;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public String getTheName(@PathParam("id") int id){
+        JsonTest test = tests.get(id);
+        if(test == null){
+            test = new JsonTest();
+            test.setId(id);
+            test.setName("Unknown " + Integer.toString(id));
+        }
+        return test.getName();
+    }
+
+
+}
+
+class JsonTest{
+    private int id;
+    private String name;
+    public int getId(){
+        return this.id;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
+
+class MyResponse{
+    public String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
